b88f07d2c6ce964a0275f5af70862ae1a6bf1f91
abhik <webdeveloper30112000@gmail.com>
2024-08-07 21:42:58 +0530

    K-Means CLustering

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+# Import necessary libraries
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.datasets import make_blobs
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Generate synthetic data
+X, y_true = make_blobs(n_samples=300, centers=4, cluster_std=0.60, random_state=0)
 
+# Visualize the data
+plt.scatter(X[:, 0], X[:, 1], s=50)
+plt.title('Generated Data')
+plt.xlabel('Feature 1')
+plt.ylabel('Feature 2')
+plt.show()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Initialize KMeans with the number of clusters
+kmeans = KMeans(n_clusters=4)
 
+# Fit the model to the data
+kmeans.fit(X)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Predict the cluster labels
+y_kmeans = kmeans.predict(X)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Plot the clustered data
+plt.scatter(X[:, 0], X[:, 1], c=y_kmeans, s=50, cmap='viridis')
+
+# Plot the cluster centers
+centers = kmeans.cluster_centers_
+plt.scatter(centers[:, 0], centers[:, 1], c='red', s=200, alpha=0.75)
+plt.title('K-Means Clustering')
+plt.xlabel('Feature 1')
+plt.ylabel('Feature 2')
+plt.show()
